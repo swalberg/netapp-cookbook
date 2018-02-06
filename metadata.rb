6fefa062b65991fb8bbcf44bf84b8f05d7684e14
JJ Asghar <jj@chef.io>
2018-02-06 14:53:54 -0600

    0.4.0 release
    - 0.4.0 release
    
    Signed-off-by: JJ Asghar <jj@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,9 @@ maintainer_email 'partnereng@chef.io'
 license          'Apache 2.0'
 description      'Manages NetApp AWS and Clustered Data ONTAP resources'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.4.0'
+
+chef_version '>= 12'
+
+issues_url 'https://github.com/chef-partners/netapp-cookbook/issues'
+source_url 'https://github.com/chef-partners/netapp-cookbook/'
